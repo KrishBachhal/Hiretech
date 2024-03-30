8f5adf07207542f9564aa7a014a03e325a9cbd32
KrishBachhal <152398294+KrishBachhal@users.noreply.github.com>
2024-03-30 15:37:03 +0530

    improve

--- My Website_files/two_bg_vanilla.js
+++ My Website_files/two_bg_vanilla.js
@@ -1,53 +1,55 @@
 window.onload = function () {
     two_replace_backgrounds();
 };
-window.addEventListener( 'elementor/frontend/init', () => {
-    elementorFrontend.hooks.addAction( 'frontend/element_ready/global', function( $scope ) {
-        $scope.find("*").addClass("two_elementor_element");
-        two_replace_backgrounds($scope.get( 0 ));
-    } );
-} );
+
+window.addEventListener('elementor/frontend/init', () => {
+    elementorFrontend.hooks.addAction('frontend/element_ready/global', function ($scope) {
+        addTwoElementorElementClass($scope);
+        two_replace_backgrounds($scope.get(0));
+    });
+});
 
 function two_replace_backgrounds(elementor_elements = false) {
     let two_elements_list;
-    if(!elementor_elements){
-        two_elements_list = document.querySelectorAll("*:not(br):not(hr):not(iframe):not(pre)");
-    }else{
-        two_elements_list = elementor_elements.querySelectorAll(".two_elementor_element");
+    if (!elementor_elements) {
+        two_elements_list = getTwoElementsList();
+    } else {
+        two_elements_list = elementor_elements.querySelectorAll('.two_elementor_element');
     }
-    two_elements_list.forEach((elem) => {
-        let style = elem.currentStyle || window.getComputedStyle(elem, false);
 
-        let bg_image = style.backgroundImage;
+    for (const elem of two_elements_list) {
+        const style = getComputedStyle(elem, null);
+        const bg_image = style.backgroundImage;
 
         if (bg_image === 'none' || bg_image.indexOf(window['two_svg_placeholder']) === -1) {
-            return;
+            continue;
         }
 
-        bg_image = bg_image.replace(window['two_svg_placeholder'], "");
+        bg_image = bg_image.replace(window['two_svg_placeholder'], '');
         if (!bg_image) {
-            return;
+            continue;
         }
 
-        elem.classList.add("two_bg");
-        elem.classList.add("lazy");
-        elem.classList.remove("two_elementor_element");
-        elem.setAttribute("data-bg-multi", bg_image);
-    });
+        addTwoBgClass(elem);
+        addLazyClass(elem);
+        removeTwoElementorElementClass(elem);
+        setDataBgMultiAttribute(elem, bg_image);
+    }
 
-    if (typeof two_lazyLoadInstance === "undefined") {
-         window.two_lazyLoadInstance = new LazyLoad({
-            'callback_applied': function(element, instance){
-                let settings = instance._settings;
-                var bgDataValue = element.getAttribute("data-" + settings.data_bg_multi);
+    if (typeof two_lazyLoadInstance === 'undefined') {
+        window.two_lazyLoadInstance = new LazyLoad({
+            'callback_applied': function (element, instance) {
+                const settings = instance._settings;
+                const bgDataValue = element.getAttribute('data-' + settings.data_bg_multi);
+
                 if (!bgDataValue) {
                     return;
                 }
 
-                if(window.getComputedStyle(element).getPropertyValue("background-image") !== bgDataValue) {
-                    let style = element.getAttribute("style");
-                    style += "background-image: " + bgDataValue + " !important;";
-                    element.setAttribute("style", style);
+                if (getComputedStyle(element).getPropertyValue('background-image') !== bgDataValue) {
+                    const style = element.getAttribute('style');
+                    style += `background-image: ${bgDataValue} !important;`;
+                    element.setAttribute('style', style);
                 }
             }
         });
@@ -56,3 +58,16 @@ function two_replace_backgrounds(elementor_elements = false) {
     }
 }
 
+function addTwoElementorElementClass($scope) {
+    $scope.find('*').addClass('two_elementor_element');
+}
+
+function getTwoElementsList() {
+    return document.querySelectorAll('*:not(br):not(hr):not(iframe):not(pre)');
+}
+
+function addTwoBgClass(elem) {
+    elem.classList.add('two_bg');
+}
+
+function addLazyClass(elem
