9c6927c7a481f1777799226baaad6077c0e9414b
KrishBachhal <krish.jatt4@gmail.com>
2024-03-30 11:25:07 +0530

    first commit

---
+++ My Website_files/lo.js
@@ -0,0 +1,16 @@
+var customData = {
+  'userID' : 1840063,
+  'Plugin' : '10web_manager'
+};
+
+window._loq = window._loq || [];
+window._loq.push (['custom', customData]);
+window._loq.push(["tag", "10web-manager"]);
+
+window.__lo_site_id = 115192;
+
+ (function() {
+  var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+  wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+   })();
